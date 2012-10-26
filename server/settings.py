ddd4b229ff8c9bb544cef54bb7f0fffb01457f44
ommicom <oroshii@gmail.com>
2012-10-26 13:46:39 +0700

    0.7.2

--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,4 @@
-http_settings = {'server':'127.0.0.1', 'port':5000}
+http_settings = {'server':'', 'port':5000}
 mongo_settings = {'server':'127.0.0.1', 'port':27017, 'dbname':'statistics'}
 log_settings = {'level':'info', 'outlet':'con'}
 table_val = {}
@@ -14,7 +14,7 @@ _graf_val = ({'param':'mem.virtual', 'format':'{0:.0f}', 'act':None},{'param':'m
             {'param':'localTime', 'format':'{0:.0f}', 'act':None})
 
 graf_val = (
-             {"group":"backgroundFlushing", "yaxis":("backgroundFlushing.flushes",),"xaxis":"localTime"},
-             {"group":"globalLock", "yaxis":("globalLock.currentQueue.total", "globalLock.currentQueue.readers","globalLock.currentQueue.total"),"xaxis":"localTime"},
-             {"group":"memory", "yaxis":("mem.virtual", "mem.mapped"),"xaxis":"localTime"},
+             {"group":"backgroundFlushing", "yaxis":("backgroundFlushing.flushes",),"xaxis":"localTime", "xdt": True},
+             {"group":"globalLock", "yaxis":("globalLock.currentQueue.total", "globalLock.currentQueue.readers","globalLock.currentQueue.total"),"xaxis":"localTime", "xdt":True},
+             {"group":"memory", "yaxis":("mem.virtual", "mem.mapped"),"xaxis":"localTime", "xdt":True},
             )
