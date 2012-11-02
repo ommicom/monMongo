8aa8f325ff055afe2351f3ef93e0298479237f0f
ommicom <oroshii@gmail.com>
2012-11-03 01:46:54 +0700

    @

--- client/settings.py
+++ client/settings.py
@@ -1,4 +1,4 @@
 mongo_settings = {'server':'127.0.0.1', 'port':27017}
 http_settings = {'server':'127.0.0.1', 'port':5000, 'method':'PUT_STAT'}
-common = {'cycle':60}
+common = {'cycle':20}
 log_settings = {'level':'info', 'outlet':'con'}
